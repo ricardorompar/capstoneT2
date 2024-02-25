1d1a658ecfc92a60e7c8a41200888515c03ac7ae
ricardorompar <ricardorompar@hotmail.com>
2024-02-26 00:36:28 +0100

    removed some useless comments that were in PastValues

--- milestones/stocks_client/src/components/PastValues.js
+++ milestones/stocks_client/src/components/PastValues.js
@@ -3,41 +3,6 @@ import React, { useState, useEffect } from 'react';
 
 function PastValues({data, showPValues}) {  //this component receives a key as a parameter like AMZN
 
-    // const [list, setList] = useState(null);
-    // useEffect(()=>{
-    //     fetch(`http://localhost:5000/api/portfolio/${key}`)
-    //     .then(response => response.json())
-    //     .then(list => setList(list))
-    //     .catch(error => console.error("Error fetching data: ", error));
-    // }, []);
-
-
-    const list = {
-        "symbol": "AMZN",
-        "stocks_daily": {  
-            "2024-02-22": {
-                "1. open": "173.1000",
-                "2. high": "174.8000",
-                "3. low": "171.7700",
-                "4. close": "174.5800",
-                "5. volume": "55392354"
-            },
-            "2024-02-21": {
-                "1. open": "168.9400",
-                "2. high": "170.2300",
-                "3. low": "167.1400",
-                "4. close": "168.5900",
-                "5. volume": "44575623"
-            },
-            "2024-02-20": {
-                "1. open": "167.8300",
-                "2. high": "168.7100",
-                "3. low": "165.7400",
-                "4. close": "167.0800",
-                "5. volume": "41980326"
-            }
-        }
-    }
     var stringData = JSON.stringify(data)
     return (
         <Container>
@@ -71,32 +36,6 @@ function PastValues({data, showPValues}) {  //this component receives a key as a
                         </ListGroup>
                 </Card>
             ) }
-            {/* {!list ? (
-                    
-                ):(
-                    <Card>
-                        <Card.Title>{list.symbol}</Card.Title>
-                        <ListGroup variant='flush'>
-                            {
-                                Object.entries(list.values_daily).map(([date, values]) =>( //this will map the contents of the json into a ListGroup.item
-                                    <ListGroup.Item key={date}>
-                                        <p className='fs-3'>{date}</p>
-                                        {   //in this case the value is another object:
-                                            Object.entries(values).map(([key, price]) =>(
-                                                <Row>
-                                                    <Col className='col-auto me-auto'>{key}</Col>
-                                                    <Col className='col-auto'>${price}</Col>
-                                                </Row>
-                                            ))
-                                        }
-                                        
-                                    </ListGroup.Item>
-                                ))
-                            }
-                        </ListGroup>
-                    </Card>
-                )
-            } */}
         </Container>
     );
 }
